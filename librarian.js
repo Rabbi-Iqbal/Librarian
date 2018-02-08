3bbf0839b836d54e0e1c4fa3731a2074e80520c3
Rabbi Iqbal <rabbi425@gmail.com>
2018-02-08 14:33:01 +0600

    Tasks 1,2,3,4 done

--- librarian.js
+++ librarian.js
@@ -3,7 +3,8 @@ var library = [
   { title: "The Old Man and the Sea", year: "1952", author: "Ernest Hemingway", publisher: "Charles Scribner's Sons" },
   { title: "Mythos: A Retelling of the Myths of Ancient Greece", year: "1981", author: "Stephen Fry", publisher: "Penguin" },
   { title: "The Mythical Man-Month", year: "1975", author: "Fred Brooks", publisher: "Addison-Wesley" },
-  { title: "The Pragmatic Programmer", year: "1999", author: "Andy Hunt and Dave Thomas", publisher: "The Pragmatic Bookshelf	" }
+  { title: "The Pragmatic Programmer", year: "1999", author: "Andy Hunt and Dave Thomas", publisher: "The Pragmatic Bookshelf	" },
+  { title: "The Alchemist", year: "1988", author: "Paulo Coelho", publisher: "HarperTorch" }
   /* Task 2 */
 ];
 
@@ -33,7 +34,20 @@ console.log(addBook(newBook));
 /* Task 4 */
 function bookByAuthor(){
 
-  //add code
+  library.sort(function(a, b){
+    var a_name = a.author.toLowerCase();
+    var b_name = b.author.toLowerCase();
+    if(a_name > b_name){
+      return 1;
+    }
+    else if (a_name < b_name) {
+      return -1;
+    }
+    else {
+      return 0;
+    }
+  })
+  return library;
 }
 console.log(bookByAuthor());
 
